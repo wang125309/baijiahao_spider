8a4e4f0cfa903ba0808156e0e745d90986e80bf6
wang125309 <wang_125309@foxmail.com>
2017-08-21 16:01:38 +0800

    view

--- backend/views.py
+++ backend/views.py
@@ -328,7 +328,7 @@ def get_total(request):
         'data' : {
             'cnt' : cnt_baijiahao,
             'op_cnt' : op_cnt,
-            'same' : float((same+weight))/float(op_cnt),
+            'same' : str(float(int(float((same+weight))/float(op_cnt)*100)/100)),
             'weight' : weight
         }
     })
